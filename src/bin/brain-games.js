27ef6119d7ba58bfc4119c5d64560ac9fb2af034
DaggLo <e.a.degtev@gmail.com>
2019-12-02 16:38:44 +0000

    feat: Add a query of user name via readline-sync.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import main from '..';
+
 console.log('Welcome to the Brain Games!');
+
+const start = main();
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+  const userName = readlineSync.question('Hi! What is your name?: ');
+
+  console.log(`Hello, ${userName}!`);
+  return userName;
+}
+
+export default greeting;
