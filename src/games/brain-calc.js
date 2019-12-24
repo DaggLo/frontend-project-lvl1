3d7a858a2b7911e48f4c71956e72292fa8a48b03
DaggLo <e.a.degtev@gmail.com>
2019-12-24 15:17:55 +0000

    style: Add some comments into brain-calc.js.

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -5,15 +5,18 @@ import random from '../utils';
 export default () => {
   // These values for the random funcntion.
   const beginValue = 0;
-  const endValue = 100;
+  const endValue = 100; // 0 - 99
 
-  const operand1 = random(beginValue)(endValue); // 0 - 99
+  const operand1 = random(beginValue)(endValue);
   const operand2 = random(beginValue)(endValue);
   const operator = random(beginValue)(endValue) / 33; // 0 - 3
 
   let question;
   let correctAnswer;
 
+  // The block below provides randomization of an operator.
+  // For example, if operator > 2 - the result operation will be summing.
+  // And so on.
   if (operator > 2) {
     question = `${operand1} + ${operand2}`;
     correctAnswer = `${operand1 + operand2}`;
