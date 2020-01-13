ebf6cc67daa4cde0153d667487545113af4ead32
DaggLo <e.a.degtev@gmail.com>
2020-01-13 10:26:34 +0000

    refactor: Move the isPrime func into the brain-prime module.

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,6 +1,20 @@
 import { cons } from '@hexlet/pairs';
 
-import random, { isPrime } from '../utils';
+import random from '../utils';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 export default () => {
   // These values for the random funcntion.
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,3 @@
-const isPrime = (num) => {
-  if (num < 2) {
-    return false;
-  }
-
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 const random = (beginValue, endValue) => Math.floor(Math.random() * endValue) + beginValue;
 
 const gcd = (x, y) => {
@@ -29,7 +15,6 @@ const gcd = (x, y) => {
 };
 
 export {
-  isPrime,
   gcd,
   random as default,
 };
