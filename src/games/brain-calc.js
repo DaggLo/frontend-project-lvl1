61def9aefacd59e255b26cd2844b42d34bcf935f
DaggLo <e.a.degtev@gmail.com>
2020-01-17 16:36:21 +0000

    refactor: Remove some magic numbers.

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -9,31 +9,37 @@ const maxRounds = 3;
 // These values are for the random funcntion.
 const beginValue = 0;
 const endValue = 100; // 0 - 99
+const operations = '+-*';
 
 const game = () => {
   const operand1 = random(beginValue, endValue);
   const operand2 = random(beginValue, endValue);
-  const operator = random(beginValue, endValue) / 33; // 0 - 3
+  const operator = operations[Math.floor(random(beginValue, endValue) / 34)]; // 0 - 2
 
-  let question;
+  const question = `${operand1} ${operator} ${operand2}`;
   let correctAnswer;
 
   // This block below provides randomization of an operator.
   // For example, if operator > 2 - the result operation will be summing.
   // And so on.
-  if (operator > 2) {
-    question = `${operand1} + ${operand2}`;
-    correctAnswer = `${operand1 + operand2}`;
-  } else if (operator > 1) {
-    question = `${operand1} - ${operand2}`;
-    correctAnswer = `${operand1 - operand2}`;
-  } else {
-    question = `${operand1} * ${operand2}`;
-    correctAnswer = `${operand1 * operand2}`;
+  switch (operator) {
+    case '+':
+      correctAnswer = `${operand1 + operand2}`;
+      break;
+
+    case '-':
+      correctAnswer = `${operand1 - operand2}`;
+      break;
+
+    case '*':
+      correctAnswer = `${operand1 * operand2}`;
+      break;
+
+    default:
+      break;
   }
 
   const data = cons(question, correctAnswer);
-
   return data;
 };
 
