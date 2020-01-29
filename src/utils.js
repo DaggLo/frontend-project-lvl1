2d2c1bbd6f54b4616806d0ef29c71a2fc3c28420
DaggLo <e.a.degtev@gmail.com>
2020-01-29 14:23:34 +0000

    add: Slightly change behavior of the getRandomValue func.
    - The maximum is inclusive and the minimum is inclusive.
    - The function work is now more consistent.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getRandomValue = (beginValue, endValue) => Math.floor(Math.random() * endValue) + beginValue;
+const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandomValue;
